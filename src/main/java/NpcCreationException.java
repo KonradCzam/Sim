d31f3cc3eae46121887cca203b61d73b85e7b169
KonradCzam <konradcz.sluzbowy@gmail.com>
2019-11-28 17:38:35 +0100

    Commit60

--- src/main/java/NpcCreationException.java
+++
@@ -1,8 +0,0 @@
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class NpcCreationException extends Exception {
-    String message;
-}
--- src/main/java/com/example/Sim/Model/DetailsInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.Sim.Model;
-
-public interface DetailsInterface {
-    public String name = "Something Went wrong";
-    public Integer value = 0;
-    public Double progress = 0.0;
-
-    public Double getProgress();
-}
